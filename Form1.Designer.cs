d84c907ea493c26bce9ba8f9239bb6f7d59eafa7
Laboratorios de Informática IES 21 <laboratoriosies@hotmail.com>
2023-09-21 10:49:10 -0300

    ...

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -64,7 +64,7 @@ namespace Repaso_Parcial
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 19);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(129, 13);
             this.label1.TabIndex = 2;
